d95571d278d63418c6dda00bfdf986deacd1e402
YouRa <allakhverdovyuriy@gmail.com>
2019-10-16 10:17:51 +0300

    Add tubes

--- core/src/com/uraall/flappybirdclone/FlappyBird.java
+++ core/src/com/uraall/flappybirdclone/FlappyBird.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+
 public class FlappyBird extends ApplicationAdapter {
 	SpriteBatch batch;
 
@@ -17,6 +18,10 @@ public class FlappyBird extends ApplicationAdapter {
 	float fallingSpeed = 0;
 	int gameStateFlag = 0;
 
+	Texture topTube;
+	Texture bottomTube;
+	int spaceBetweenTubes = 500;
+
 	
 	@Override
 	public void create () {
@@ -27,10 +32,17 @@ public class FlappyBird extends ApplicationAdapter {
 		bird[1] = new Texture("bird_wings_down.png");
 		flyHeight = Gdx.graphics.getHeight() / 2
 				- bird[0].getHeight() / 2;
+
+		topTube = new Texture("top_tube.png");
+		bottomTube = new Texture("bottom_tube.png");
 	}
 
 	@Override
 	public void render () {
+
+		batch.begin();
+		batch.draw(background, 0, 0, Gdx.graphics.getWidth(),
+				Gdx.graphics.getHeight());
 
         if (Gdx.input.justTouched()) {
             Gdx.app.log("Tap", "Oops!");
@@ -38,6 +50,14 @@ public class FlappyBird extends ApplicationAdapter {
         }
 
 		if (gameStateFlag == 1) {
+
+			batch.draw(topTube, Gdx.graphics.getWidth() / 2
+					- topTube.getWidth() / 2, Gdx.graphics.getHeight() / 2 +
+					spaceBetweenTubes / 2);
+			batch.draw(bottomTube, Gdx.graphics.getWidth() / 2
+					- bottomTube.getWidth() / 2, Gdx.graphics.getHeight() / 2 -
+					spaceBetweenTubes / 2 - bottomTube.getHeight());
+
 			if (Gdx.input.justTouched()) {
 				fallingSpeed = -30;
 			}
@@ -61,9 +81,7 @@ public class FlappyBird extends ApplicationAdapter {
 			birdStateFlag = 0;
 		}
 
-		batch.begin();
-		batch.draw(background, 0, 0, Gdx.graphics.getWidth(),
-				Gdx.graphics.getHeight());
+
 		batch.draw(bird[birdStateFlag], Gdx.graphics.getWidth() / 2
 						- bird[birdStateFlag].getWidth() / 2,
 				flyHeight);
